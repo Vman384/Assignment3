6a3fc0fb57c90a4047e2055bc3e413e927ab288a
MahdeeIslam <108563754+MahdeeIslam@users.noreply.github.com>
2023-05-28 12:30:00 +1000

    Violation of a time complexity

--- balancing.py
+++ balancing.py
@@ -5,22 +5,48 @@ import random
 from threedeebeetree import ThreeDeeBeeTree, BeeNode
 from ratio import Percentiles
 
-
 def make_ordering(my_coordinate_list: list[Point]) -> list[Point]:
+    """
+
+    Complexity :
+
+    Best case = Worst case : The nested loops in make_ordering_rec are executed for each point in the input list. 
+                             This operation leads to a cubic time complexity of O(n^3). Adding points to the Percentiles 
+                             object contributes a complexity of O(n log n) since each insertion into a Binary Search Tree is 
+                             O(log n) and this operation is repeated n times. The ratio calculation function traverses through 
+                             all nodes of the binary tree, giving a time complexity of O(n). Therefore, adding up all these 
+                             operations, the total time complexity of the make_ordering function is O(n^3) + O(n log n) + O(n). 
+                             However, as n grows large, the terms O(n log n) and O(n) become insignificant relative to the cubic 
+                             term. So, it is common to represent the overall time complexity using the term with the highest order 
+                             of magnitude, which in this case is O(n^3).
+
+                             ( yes yes i know O(n^3) is insane )
+
+
+    """
     def make_ordering_rec(order : list[Point]) -> tuple[float,float,float]:
         n = len(order)
         score = []
         number_of_points=3
         for i in range(number_of_points):
-            left = max(0, i-1)  # Look at the neighbor on the left
-            right = min(i+1, number_of_points-1)  # Look at the neighbor on the right
-            left_score = left / (i + 1) if order[i] > order[left] else 0
-            right_score = (n - right) / (n - i) if order[i] > order[right] else 0
-            score.append(max(left_score, right_score))
-        return tuple(score)
-
-    my_coordinate_list.sort()  # Sort the list before processing
-
+            left = [0] * number_of_points
+            right = [0] * number_of_points
+            for j in range(number_of_points):
+                if i==j:
+                    continue
+                if order[j] < order[i]:
+                    left[i]+=1
+                else:
+                    right[i]+=1
+            final_score = []
+            for _ in range(number_of_points):
+                if min(left[i] , right[i]) > 17:
+                    final_score.append(min(left[i] , right[i]) / max(left[i] , right[i]))
+                if not final_score:
+                    score.append(1.0)
+                else:
+                    score.append(min(final_score))
+            return tuple(score)
     best = (float('inf') , float('inf') , float('inf'))
     for compute in my_coordinate_list:
         new_score = make_ordering_rec(compute)
@@ -34,7 +60,6 @@ def make_ordering(my_coordinate_list: list[Point]) -> list[Point]:
     my_coordinate_list = ratio.ratio(0,0)
 
     return my_coordinate_list[0:7]
-
 
 
         
